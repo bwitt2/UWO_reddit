96522b3cdde7752604824cea12ec4c2cc9723701
Eric <esteve5@uwo.ca>
2014-09-13 18:55:45 -0400

    Set up templates

--- UWO_reddit/UWO_reddit/urls.py
+++ UWO_reddit/UWO_reddit/urls.py
@@ -3,10 +3,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from UWO_reddit_app import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'UWO_reddit.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index')
 )
--- UWO_reddit/UWO_reddit_app/views.py
+++ UWO_reddit/UWO_reddit_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
-def main(request):
-	template = loader.get_template()
+
+def index(request):
+	return render(request, 'UWO_reddit_app/index.html')
 
