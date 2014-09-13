7c822319ec6b51d5841925746a2fd4618403a883
Brandon Witt <bwitt2@uwo.ca>
2014-09-13 19:04:36 -0400

    started the first view.

--- UWO_reddit/UWO_reddit_app/views.py
+++ UWO_reddit/UWO_reddit_app/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
-
+from UWO_reddit.models import *
 def index(request):
-	return render(request, 'UWO_reddit_app/index.html')
+	ls = Post
+	template = loader.get_template('UWO_reddit_app/index.html')
+	context = RequestContext(request, {
+		'posts' : ls,
+		})
+	return HttpResponse(template.render(context))
 
