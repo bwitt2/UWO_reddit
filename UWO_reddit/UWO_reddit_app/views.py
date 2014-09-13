b5aad2c96dfc363953629e40b7309256af1f1fc9
Brandon Witt <bwitt2@uwo.ca>
2014-09-13 18:11:10 -0400

    started views.

--- UWO_reddit/UWO_reddit_app/views.py
+++ UWO_reddit/UWO_reddit_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
-# Create your views here.
+def main(request):
+	template = loader.get_template()
+
