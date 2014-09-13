64edc9187e8e7f0e607cf8df1d5c8155793f9642
Eric <esteve5@uwo.ca>
2014-09-13 18:25:35 -0400

    Added unicode representation for models

--- UWO_reddit/UWO_reddit_app/models.py
+++ UWO_reddit/UWO_reddit_app/models.py
@@ -5,11 +5,17 @@ from django.db import models
 class Category(models.Model):
 	name = models.CharField(max_length=100)
 
+	def __unicode__(self):
+		return self.name
+
 
 class SubCategory(models.Model):
 	category = models.ForeignKey(Category)
 	name = models.CharField(max_length=100)
 	description = models.CharField(max_length=400)
+
+	def __unicode__(self):
+		return self.name
 
 
 class Post(models.Model):
@@ -18,8 +24,14 @@ class Post(models.Model):
 	time_posted = models.DateTimeField('Time Posted', auto_now_add=True)
 	content = models.TextField()
 
+	def __unicode__(self):
+		return self.title
+
 
 class Comment(models.Model):
 	post = models.ForeignKey(Post)
 	time_commented = models.DateTimeField('Time Commented', auto_now_add=True)
 	content = models.TextField()
+
+	def __unicode__(self):
+		return selt.content
