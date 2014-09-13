a730c1d80848b80219f6e8836866115b6268e420
Eric <esteve5@uwo.ca>
2014-09-13 19:22:43 -0400

    fixed templates

--- UWO_reddit/UWO_reddit_app/views.py
+++ UWO_reddit/UWO_reddit_app/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
-from UWO_reddit.models import *
+from UWO_reddit_app.models import *
 def index(request):
-	ls = Post
+	ls = Post.objects.all()
 	template = loader.get_template('UWO_reddit_app/index.html')
 	context = RequestContext(request, {
 		'posts' : ls,
